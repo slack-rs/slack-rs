c261134d2f53f63faf225ca9c4b854cfbf92dfe6
BenTheElder <bentheelder@gmail.com>
2016-03-20 16:55:26 -0400

    Update copyright boilerplate and add AUTHORS.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //
-// Copyright 2014 Benjamin Elder from https://github.com/BenTheElder/slack-rs
+// Copyright 2014-2016 the slack-rs authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
