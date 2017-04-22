07d25fa71c7cbccf76a0acd095fa679d4dd96104
Christopher Brickley <brickley@gmail.com>
2017-04-22 18:09:28 -0400

    chore(future): rm unneeded type specification

--- examples/future.rs
+++ examples/future.rs
@@ -18,7 +18,7 @@ fn main() {
 
         fn on_event(&mut self,
                     _cli: &mut Client,
-                    event: ::std::result::Result<Event, slack::Error>)
+                    event: Result<Event, slack::Error>)
                     -> Self::EventFut {
             if let Ok(event) = event {
                 println!("event = {:#?}", event);
